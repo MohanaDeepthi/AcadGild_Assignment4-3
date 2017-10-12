4b3f9a26cbd23f7d82764f24faf7217fc539da2a
Deepthi <deepthi.vm@gmail.com>
2017-10-12 13:59:46 -0400

    commit

---
+++ src/assignment4_3/FinalVariableDemo.java
@@ -0,0 +1,11 @@
+package assignment4_3;//package name
+
+public class FinalVariableDemo {
+	
+	final int countVar = 0;//declaring variable as final
+
+	public FinalVariableDemo() {
+		countVar++; //The final field FinalVariableExample.count cannot be assigned
+	}
+
+}
